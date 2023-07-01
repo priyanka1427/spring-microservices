58cf2f6ac29520c17d6669d6dd014f40154a69f7
Rajesh K <karnatirajesh0@gmail.com>
2023-07-01 18:40:08 +0530

    Intial commit

---
+++ src/test/java/com/microservices/microservicesproject/MicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.microservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
